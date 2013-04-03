749cdaccefaad63704b953b370bcd302d6c325b0
Maciej Paliński <maciej.palinski@netguru.pl>
2013-04-03 23:26:40 +0200

    new syntax for attributes post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
   attr_accessible :content, :title, :category_id, :slug, :hidden, :tag_list
-  acts_as_url :title, :sync_url => true, :url_attribute => :slug
+  acts_as_url :title, sync_url: true, url_attribute: :slug
   acts_as_taggable_on :tags
 
   validates :content, :title, presence: true
@@ -10,7 +10,7 @@ class Post < ActiveRecord::Base
 
   default_scope order("published DESC")
 
-  scope :visible, where(:hidden => false)
+  scope :visible, where(hidden: false)
   scope :published, where("posts.published IS NOT NULL")
 
   def to_param
