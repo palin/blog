f2297f7ef8fa1afc12d978346c6e8bd181564661
Maciej Palinski <maciekpalinski@gmail.com>
2014-02-17 21:51:48 +0000

    limit expose

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,8 +5,7 @@ class PostsController < ApplicationController
   expose(:pgn) { Pagination.new(posts, 3, params) }
   expose(:posts_paginated) { posts.page(params[:page] || 1).per(params[:per_page] || 10) }
   expose(:decorated_posts) { PostDecorator.decorate_collection(posts_paginated) }
-  expose(:raw_post) { Post.find_by_slug(params[:id])}
-  expose(:post) { raw_post.decorate }
+  expose(:post) { Post.find_by_slug(params[:id]).decorate }
   expose(:comment) { Comment.new }
   expose(:comments) { post.comments }
   expose(:captcha) { setup_negative_captcha }
